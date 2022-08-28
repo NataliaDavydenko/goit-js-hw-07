56c79f68db5b16e65fb5722d08308f024695236b
Natalia <davydenko.n@ukr.net>
2022-08-28 19:58:54 +0300

    added libraries and js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const gallery = (image) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItems.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`;
+};
+
+const galleryElement = galleryItems.map(gallery).join("");
+galleryList.insertAdjacentHTML("beforeend", galleryElement);
+
+galleryList.addEventListener("click", onModalOpen);
+
+function onModalOpen(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const imgLink = document.querySelector(".gallery__link");
+  imgLink.href = event.target.dataset.source;
+
+  const modalInstance = basicLightbox.create(`<div class="modal">
+        <img class="modal__image" src="${imgLink.href}" height="650"/>
+    </div>`);
+
+  modalInstance.show();
+
+  window.addEventListener("keydown", onModalClose);
+
+  function onModalClose(event) {
+    if (event.key === "Escape") {
+      modalInstance.close();
+      window.removeEventListener("keydown", onModalClose);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+function onElementsCreate(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+ </a>`;
+    })
+    .join("");
+}
+
+const galleryElements = onElementsCreate(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryElements);
+
+galleryList.addEventListener("click", openImgEl);
+
+function openImgEl(event) {
+  event.preventDefault();
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  sourceAttr: "href",
+  captionsData: "alt",
+  captionDelay: 250,
+});
